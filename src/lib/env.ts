cdaaf84a54beaf2f603ccf5b075b02ba258689fd
Git4Prg <parigyan.sroy@gmail.com>
2026-04-05 18:33:02 +0530

    backend infrastructure

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,6 +4,11 @@ import { createEnv } from "@t3-oss/env-nextjs";
 export const env = createEnv({
     server: {
         DATABASE_URL: z.string().min(1),
+        APP_URL: z.string().min(1),
+        AWS_ACCESS_KEY_ID: z.string().min(1),
+        AWS_SECRET_ACCESS_KEY: z.string().min(1),
+        AWS_REGION: z.string().min(1),
+        AWS_BUCKET_NAME: z.string().min(1),
     },
     experimental__runtimeEnv: {},
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
